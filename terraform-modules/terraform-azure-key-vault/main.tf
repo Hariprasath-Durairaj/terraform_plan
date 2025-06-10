0c0e01ad5a83af2fb2fd6d52863982c8c57acb6e
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-10 15:11:34 -0400

    Update main.tf

--- terraform-modules/terraform-azure-key-vault/main.tf
+++ terraform-modules/terraform-azure-key-vault/main.tf
@@ -12,6 +12,12 @@ resource "azurerm_key_vault" "this" {
   enabled_for_template_deployment = var.enabled_for_template_deployment
   purge_protection_enabled   = var.purge_protection_enabled
   soft_delete_retention_days = var.soft_delete_retention_days
+  resource "azurerm_key_vault_secret" "appgw_pfx" {
+  name         = "appgw-pfx"
+  vault_uri    = module.key_vault.vault_uri
+  value        = filebase64("${path.module}/certs/appgw.pfx")
+  content_type = "application/x-pkcs12"
+}
 
   network_acls {
     default_action             = var.default_action
