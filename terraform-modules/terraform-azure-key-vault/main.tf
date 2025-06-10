e87b41fb02b7e3c21f55031004fd1396f33472e1
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-09 22:52:18 -0400

    Update main.tf

--- terraform-modules/terraform-azure-key-vault/main.tf
+++ terraform-modules/terraform-azure-key-vault/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_key_vault" "this" {
   resource_group_name         = var.resource_group_name
   tenant_id                   = var.tenant_id
   sku_name                    = "standard"
-  public_network_access_enabled = var.public_network_access_enable
+  public_network_access_enabled = var.public_network_access_enabled
 
   enable_rbac_authorization  = var.enable_rbac_authorization
   enabled_for_deployment     = var.enabled_for_deployment
