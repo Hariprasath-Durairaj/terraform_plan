a376ecdb7d87e6a295127f40559d257b285e01b0
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-11 16:25:21 -0400

    Update output.tf

--- terraform-modules/terraform-azure-aks/output.tf
+++ terraform-modules/terraform-azure-aks/output.tf
@@ -63,3 +63,11 @@ output "kubelet_identity_object_id" {
   description = "Object ID of the kubelet managed identity"
   value       = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
 }
+output "kube_config" {
+  value = {
+    host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+    client_certificate     = azurerm_kubernetes_cluster.this.kube_config[0].client_certificate
+    client_key             = azurerm_kubernetes_cluster.this.kube_config[0].client_key
+    cluster_ca_certificate = azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+  }
+}
