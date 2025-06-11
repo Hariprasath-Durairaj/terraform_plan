0f1f9da8f10eda35205c5a8c5552093a3080d697
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-11 16:14:14 -0400

    Update main.tf

--- environments/QA/main.tf
+++ environments/QA/main.tf
@@ -213,6 +213,13 @@ module "aks" {
   tags = var.tags
 }
 
+provider "kubernetes" {
+  host                   = module.aks.kube_config.host
+  client_certificate     = base64decode(module.aks.kube_config.client_certificate)
+  client_key             = base64decode(module.aks.kube_config.client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config.cluster_ca_certificate)
+}
+
 ############################
 # 7. Private DNS
 ############################
