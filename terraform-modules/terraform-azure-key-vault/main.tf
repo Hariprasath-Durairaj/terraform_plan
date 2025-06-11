f8a7e463d9917e461eb2121c07f4e450d9613411
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-11 15:34:33 -0400

    Update main.tf

--- terraform-modules/terraform-azure-key-vault/main.tf
+++ terraform-modules/terraform-azure-key-vault/main.tf
@@ -1,28 +1,29 @@
 resource "azurerm_key_vault" "this" {
-  name                        = var.name
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
-  public_network_access_enabled = var.public_network_access_enabled
+  name                            = var.name
+  location                        = var.location
+  resource_group_name             = var.resource_group_name
+  tenant_id                       = var.tenant_id
+  sku_name                        = "standard"
+  public_network_access_enabled   = var.public_network_access_enabled
 
-  enable_rbac_authorization  = var.enable_rbac_authorization
-  enabled_for_deployment     = var.enabled_for_deployment
-  enabled_for_disk_encryption = var.enabled_for_disk_encryption
+  enable_rbac_authorization       = var.enable_rbac_authorization
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
   enabled_for_template_deployment = var.enabled_for_template_deployment
-  purge_protection_enabled   = var.purge_protection_enabled
-  soft_delete_retention_days = var.soft_delete_retention_days
-  resource "azurerm_key_vault_secret" "appgw_pfx" {
-  name         = "appgw-pfx"
-  vault_uri    = module.key_vault.vault_uri
-  value        = filebase64("${path.module}/certs/appgw.pfx")
-  content_type = "application/x-pkcs12"
-}
+  purge_protection_enabled        = var.purge_protection_enabled
+  soft_delete_retention_days      = var.soft_delete_retention_days
 
   network_acls {
-    default_action             = var.default_action
-    bypass                     = var.bypass
+    default_action = var.default_action
+    bypass         = var.bypass
   }
 
   tags = var.tags
+}
+
+resource "azurerm_key_vault_secret" "appgw_pfx" {
+  name         = "appgw-pfx"
+  value        = filebase64("${path.module}/certs/appgw.pfx")
+  key_vault_id = azurerm_key_vault.this.id
+  content_type = "application/x-pkcs12"
 }
