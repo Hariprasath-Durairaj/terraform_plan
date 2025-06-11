bc0b99f6389b36b24fd629e9ac60c218ea4729ee
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-11 18:51:37 -0400

    Update output.tf

--- terraform-modules/terraform-azure-nsg/output.tf
+++ terraform-modules/terraform-azure-nsg/output.tf
@@ -1,6 +1,5 @@
 output "nsg_id" {
-  description = "The ID of the Network Security Group"
-  value       = azurerm_network_security_group.this.id
+  value = azurerm_network_security_group.this.id
 }
 output "aks_nsg_id" {
   value = azurerm_network_security_group.this.id
