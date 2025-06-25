2b98cd30b68def979b95a66540e6159977cd096f
Hariprasath Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-25 03:30:29 +0000

    Updated provider.tf

--- environments/QA/provider.tf
+++ environments/QA/provider.tf
@@ -12,11 +12,19 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.22" # matches AKS 1.29+ API versions
+      version = "~> 2.22"  # matches AKS 1.29+ API versions
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12"
+      version = "~> 2.12"  # supports AGIC Helm install
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"   # for Azure AD resources
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"   # for unique ID generation
     }
   }
 }
